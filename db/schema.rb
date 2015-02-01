a7aec4760293c1bf463e3bcd3e03cfe6ef408050
Adler <nkj20932@hotmail.com>
2015-02-01 09:53:58 +0800

    add name to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150131054734) do
+ActiveRecord::Schema.define(version: 20150201013928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,13 @@ ActiveRecord::Schema.define(version: 20150131054734) do
 
   add_index "ingredients", ["dish_id"], name: "index_ingredients_on_dish_id", using: :btree
 
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "prepare_lists", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "ingredient_id"
@@ -72,6 +79,7 @@ ActiveRecord::Schema.define(version: 20150131054734) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
